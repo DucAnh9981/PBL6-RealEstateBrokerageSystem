365a8598d09cdf0fac02eb522fcb307b4a596e25
DucAnh9981 <anhnguyeduc331@gmail.com>
2024-11-24 21:30:15 +0700

    add blabla

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -148,6 +148,13 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 CORS_ALLOW_ALL_ORIGINS = True
 # CORS_ALLOWS_CREDENTIALS = True
+ALLOWED_HOSTS = ['*']
+
+CORS_ALLOWED_ORIGIN_REGEXES = [
+    r"^http:\/\/172\.20\.10\.3(:\d+)?$",  # Cho phép từ IP với cổng bất kỳ
+    r"^http:\/\/localhost(:\d+)?$",       # Cho phép từ localhost
+    r"^http:\/\/127\.0\.0\.1(:\d+)?$",    # Cho phép từ 127.0.0.1
+]
 
 AUTH_USER_MODEL = "accounts.User"
 
